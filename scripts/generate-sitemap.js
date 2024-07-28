256dbe228359a8f43785ded40fe4e3870f2f926b
Bhavesh Patil <145092023+iambhvsh@users.noreply.github.com>
2024-07-28 22:10:36 +0530

    Update generate-sitemap.js

--- scripts/generate-sitemap.js
+++ scripts/generate-sitemap.js
@@ -39,10 +39,15 @@ const crawlSite = async (url) => {
 
 const generateSitemap = (urls) => {
   const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
-  <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+  <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
+          xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+          xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9
+          http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
     ${urls.map((url) => `
       <url>
         <loc>${url}</loc>
+        <changefreq>monthly</changefreq>
+        <priority>0.5</priority>
       </url>
     `).join('')}
   </urlset>`;
