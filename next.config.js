7ca061049771b6fc651795831f6d03270d7f643b
Bhavesh Patil <145092023+iambhvsh@users.noreply.github.com>
2024-07-28 21:18:59 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 const withNextra = require('nextra')({
   theme: 'nextra-theme-docs',
   themeConfig: './theme.config.tsx',
-});
-const { withSitemap } = require('next-sitemap');
+})
 
-module.exports = withSitemap(withNextra({
-  reactStrictMode: true,
-}));
+module.exports = withNextra()
