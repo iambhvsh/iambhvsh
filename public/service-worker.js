b12035a6cf6169c46b01b13d1e9cb4a0ea03f961
Bhavesh Patil <145092023+iambhvsh@users.noreply.github.com>
2024-11-26 16:53:26 +0530

    Create service-worker.js

---
+++ public/service-worker.js
@@ -0,0 +1,21 @@
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open('app-cache').then((cache) => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/app.js',
+        '/style.css',
+        // Add other assets to cache
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
