c1a037cf0b57e06a0011c59307a9ed7a71bd80d8
Bhavesh Patil <145092023+iambhvsh@users.noreply.github.com>
2024-12-08 23:32:08 +0530

    Update service-worker.js

--- public/service-worker.js
+++ public/service-worker.js
@@ -8,17 +8,16 @@ self.addEventListener('install', (event) => {
         '/robots.txt',
         '/projects',
         '/blog',
+        '/education',
+        '/about',
+        '/contact',
         'https://placehold.co/72x72/000000/FFFFFF/png?text=B&font=montserrat',
         'https://placehold.co/96x96/000000/FFFFFF/png?text=B&font=montserrat', 
         'https://placehold.co/128x128/000000/FFFFFF/png?text=B&font=montserrat',
         'https://placehold.co/144x144/000000/FFFFFF/png?text=B&font=montserrat',
         'https://placehold.co/152x152/000000/FFFFFF/png?text=B&font=montserrat',
         'https://placehold.co/192x192/000000/FFFFFF/png?text=B&font=montserrat',
-        'https://placehold.co/384x384/000000/FFFFFF/png?text=B&font=montserrat',
-        'https://placehold.co/1200x630/1a1a1a/FFFFFF/png?text=Pixel+Store',
-        'https://placehold.co/1200x630/1a1a1a/FFFFFF/png?text=DarkValor',
-        'https://placehold.co/1200x630/1a1a1a/FFFFFF/png?text=StackX',
-        'https://placehold.co/1200x630/1a1a1a/FFFFFF/png?text=Chat+App'
+        'https://placehold.co/384x384/000000/FFFFFF/png?text=B&font=montserrat'
       ]);
     })
   );
